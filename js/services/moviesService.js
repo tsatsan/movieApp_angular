450bd76716cf3a3ec4acf36e3644c187231a7cc7
jordiCostaFigueras <desarrolloweb@worktel.com>
2018-02-19 09:31:46 +0100

    ad service nowPlaying

--- js/services/moviesService.js
+++ js/services/moviesService.js
@@ -13,7 +13,7 @@ angular.module('pelimoviesApp')
     }
 
     function _getInComingMovies () {
-      var urlInComingMovies = 'https://api.themoviedb.org/3/movie/upcoming?api_key='+ api_key +'&language=en-US&page=1'
+      var urlInComingMovies = 'https://api.themoviedb.org/3/movie/upcoming?api_key=' +  api_key + '&language=en-US&page=1'
       return $http.get(urlInComingMovies)
     }
 
@@ -21,14 +21,22 @@ angular.module('pelimoviesApp')
       var urlMovieById = 'https://api.themoviedb.org/3/movie/' + idMovie + '?api_key=' + api_key + '&language=en-US'
       return $http.get(urlMovieById)
     }
-    
+    function _getTopRatedMovies () {
+      var urlTopRatedMovies = 'https://api.themoviedb.org/3/movie/top_rated?api_key=' + api_key + '&language=en-US&page=1'
+      return $http.get(urlTopRatedMovies)
+    }
+
+    function _getNowPlayingMovies () {
+      var urlNowPlayingMovies = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + api_key + '&language=en-US&page=1'
+      return $http.get(urlNowPlayingMovies)
+    }
 
     return {
       getMovies: _getMovies,
       getPopularMovies: _getPopularMovies,
       getInComingMovies: _getInComingMovies,
-      getMovieById: _getMovieById
+      getMovieById: _getMovieById,
+      getTopRatedMovies: _getTopRatedMovies,
+      getNowPlayingMovies: _getNowPlayingMovies
     }
   })
-
-
